f06f11fa49964bc5b7176423bc686597e8462b1a
sandy saputra <ss5679848@gmail.com>
2023-07-24 21:35:31 +0700

    update dark mode

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,35 +1,47 @@
 <template>
-  <header class="flex">
-
-    <div class="branding" style="margin-bottom: 14px;background-color:white;">
-      <img src="@/assets/profile.png" alt="profile" draggable="false" />
+  <header class="flex" :class="{ 'dark-mode': isDarkMode, 'light-mode': !isDarkMode }">
+    <div class="branding" style="margin-bottom: 14px; background-color: white">
+      <img src="@/assets/profile.png" alt="profile" draggable="true" />
     </div>
     <div class="branding flex">
       <img src="@/assets/file-invoice-dollar-solid.png" alt="menu" draggable="true" />
     </div>
-
-    <div class="branding" >
-      <img src="@/assets/profile.png" alt="profile" draggable="false" />
+    <img src="@/assets/starts.svg" style="margin-top: 10em;padding-left: 20%;"  width="70" height="30" @click="toggleDarkMode"/>
+    <div class="branding"  style="margin-top: 15em; background-color: white">
+      <img src="@/assets/profile.png" alt="profile" draggable="true" />
     </div>
   </header>
-
-
 </template>
 
 <script>
 export default {
   name: 'Navigation',
 
-
-
-
-
+  data() {
+    return {
+      isDarkMode: false // Set this to true if you want to start with dark mode by default
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
+      // You can also save the user's preference in local storage or a cookie to remember their choice across sessions.
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.dark-mode {
+  background-color: black;
+  color: white;
+}
 
-
+/* Light mode styles */
+.light-mode {
+  background-color: white;
+  color: black;
+}
 header {
   z-index: 99;
   flex-direction: row;
@@ -42,19 +54,18 @@ header {
   }
 }
 
-
-  .branding {
-    border-radius: 0 20px 20px 0;
-    background-color: #7c5dfa;
-    justify-content: center;
-    padding: 24px;
-    @media (min-width: 900px) {
-      width: 100%;
-    }
-
-    img {
-      width: auto;
-      height: 30px;
-    }
+.branding {
+  border-radius: 0 20px 20px 0;
+  background-color: #7c5dfa;
+  justify-content: center;
+  padding: 24px;
+  @media (min-width: 900px) {
+    width: 100%;
   }
+
+  img {
+    width: auto;
+    height: 30px;
+  }
+}
 </style>
